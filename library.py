c83d20126c2ab6fd47f5f49f1f80875580cd5420
kaisefromwork <kaiserfromwork@gmail.com>
2025-03-21 17:14:08 -0300

    adding books to library is working

--- library.py
+++ library.py
@@ -7,19 +7,21 @@ class Library():
         self.list_of_users = []
         self.list_of_books = []
 
+
     def add_book(self, book):
         if isinstance(book, Book):
             self.list_of_books.append(book)
         else:
             return "Object not of class Book()"
-        return f"Book {book.name} added to the library!"
+        return f"Book {book.title} added to the library!"
     
+
     def remove_book(self, book):
         if isinstance(book, Book):
             if book in self.list_of_books:
                 self.list_of_books.remove(book)
             else:
-                return f"{book.name} is not on the library list."
+                return f"{book.title} is not on the library list."
         else:
             return "Not a instance of Book() class"
     
@@ -27,14 +29,17 @@ class Library():
     def display_users(self):
         return self.list_of_users
     
+
     def display_books(self):
         return self.list_of_books
+    
     
     def find_user(self, name, surname):
         for user in self.list_of_users:
             if user.name == name and user.surname == surname:
                 return user
         return None
+    
     
     def find_book(self, book_name, book_author):
         for book in self.list_of_books:
--- main.py
+++ main.py
@@ -11,5 +11,9 @@ lucas = User("Lucas", "de Oliveira")
 # Book
 book_01 = Book("1984", "George Orwell")
 
+###################################################### - TEST - ######################################################
+library.add_book(book_01)
 
 
+for x in library.display_books():
+    print(x.title, x.author)
