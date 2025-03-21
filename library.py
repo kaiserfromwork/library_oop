b5b5aa4bfead7888f558ef7c263412f65fd84bc6
kaisefromwork <kaiserfromwork@gmail.com>
2025-03-21 17:05:30 -0300

    implemented add and remove book functions to library class

---
+++ library.py
@@ -0,0 +1,41 @@
+# Library 
+from book import Book
+
+class Library():
+
+    def __init__(self):
+        self.list_of_users = []
+        self.list_of_books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.list_of_books.append(book)
+        else:
+            return "Object not of class Book()"
+        return f"Book {book.name} added to the library!"
+    
+    def remove_book(self, book):
+        if isinstance(book, Book):
+            if book in self.list_of_books:
+                self.list_of_books.remove(book)
+            else:
+                return f"{book.name} is not on the library list."
+        else:
+            return "Not a instance of Book() class"
+    
+
+    def display_users(self):
+        return self.list_of_users
+    
+    def display_books(self):
+        return self.list_of_books
+    
+    def find_user(self, name, surname):
+        for user in self.list_of_users:
+            if user.name == name and user.surname == surname:
+                return user
+        return None
+    
+    def find_book(self, book_name, book_author):
+        for book in self.list_of_books:
+            return book if book.name == book_name and book.author == book_author else None
---
+++ main.py
@@ -0,0 +1,15 @@
+from library import Library
+from user import User
+from book import Book
+
+# Library
+library = Library()
+
+# User
+lucas = User("Lucas", "de Oliveira")
+
+# Book
+book_01 = Book("1984", "George Orwell")
+
+
+
