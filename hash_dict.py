abcbfedc8db392e8d6c4ef75ad574b96b780c6da
kaisefromwork <kaiserfromwork@gmail.com>
2025-03-27 17:21:04 -0300

    added docstring and comments to hash_dict.py

--- hash_dict.py
+++ hash_dict.py
@@ -5,10 +5,31 @@ import time
 class HashDict:
 
     def hash_dict(name, surname):
-        unique_id = f"{name}{surname}"
+        """Creates a hash string id using user's name and surname
+        
+        Keyword arguments:
+            name (str) - user's name
+            surname (str) - user's surname
+        
+        Return: 
+            Returns a hash unique id
+        """
+        
+        unique_id = f"{name}{surname}"  # String used to create unique hash id
         return hashlib.sha256(unique_id.encode()).hexdigest()
     
 
     def hash_dict_book(title, author, year):
-        unique_id = f"{title}{author}{year}{time.time()}"
+        """Creates a hash string using book's title, author and year. Used as ID
+        
+        Keyword arguments:
+            title (str) -- book's title
+            author (str) -- book's author
+            year (str) -- book's year
+
+        Return: 
+            Returns a hash string unique id
+        """
+        
+        unique_id = f"{title}{author}{year}{time.time()}" # string used to create unique hash id
         return hashlib.sha256(unique_id.encode()).hexdigest()
