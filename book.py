169df35f8b10494f5e2d9dc6973883c539b46445
kaisefromwork <kaiserfromwork@gmail.com>
2025-03-29 07:20:31 -0300

    created a subclass of book for fictions genre books

--- book.py
+++ book.py
@@ -3,10 +3,9 @@
 # Modules
 from hash_dict import HashDict
 class Book():
-    def __init__(self, title, author, year, borrowed=False):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
-        self.borrowed = borrowed
         self.year = year
         self._book_id = self.create_book_id(title, author, year)   
 
@@ -14,13 +13,26 @@ class Book():
     def create_book_id(self, title, author, year):
         return HashDict.hash_dict_book(title, author, year)
         
-    def borrow_book(self):
-        self.borrowed = True
-
 
     def get_book_id(self):
         return self._book_id
+    
+
+    def display_book(self):
+        return (f"tile : {self.title}, author: {self.author}, year: {self.year}")
 
 
     def __str__(self):
-        return self.title + self.author + self.borrowed
+        return self.title + self.author + self.year 
+
+class FictionBook(Book):
+
+    def __init__(self, title, author, year, genre="Fiction"):
+        super().__init__(title, author, year)
+        self.genre = genre
+
+
+    def display_book(self):
+        parent_class = super().display_book()
+
+        return f"{parent_class}, genre : {self.genre}"
