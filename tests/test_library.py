48f905c57603417b77ca9338acde6e1d787add6b
kaisefromwork <kaiserfromwork@gmail.com>
2025-04-03 05:40:34 -0300

    implmented library.py unit test

--- tests/test_library.py
+++ tests/test_library.py
@@ -33,9 +33,46 @@ def test_add_user(library, sample_user):
 
 def test_find_user(library, sample_user):
     library.user_db.user_database_info = {sample_user.get_user_id(): {"name": "John", "surname": "Doe"}}
+    result =  library.find_user(sample_user)
+    assert result is True
+    library.user_db.update_user_database.assert_called_once()
+
+
+def test_delete_user(library, sample_user):
+    library.user_db.user_database_info = {sample_user.get_user_id(): {"name": "John", "surname": "Doe"}}
     result =  library.delete_user(sample_user)
     assert result is True
     library.user_db.update_user_database.assert_called_once()
 
 
-# TODO: Finish testing
+def test_add_book(library, sample_book):
+    library.books_db.book_database_info = {}
+    result = library.add_book(sample_book)
+    assert result is True
+    library.books_db.update_book_database.assert_called_once()
+
+
+def test_remove_book(library, sample_book):
+    library.books_db.book_database_info = {sample_book.get_book_id(): {"title": "1984", "author": "George Orwell", "year": "1948"}}
+    result = library.remove_book(sample_book)
+    assert result is True
+    library.books_db.update_book_database.assert_called_once()
+
+
+def test_find_book(library, sample_book):
+    library.books_db.book_database_info = {sample_book.get_book_id(): {"title": "1984", "author": "George Orwell", "year": "1948"}}
+    result = library.find_book("1984", "George Orwell", "1948")
+    assert result == {"title": "1984", "author": "George Orwell", "year": "1948"}
+
+
+def test_borrow_book(library, sample_book, sample_user):
+    library.borrowed_books_db.borrowed_books_data = {}
+    result = library.borrow_book(sample_book, sample_user)
+    assert result is True
+    library.borrowed_books_db.update_borrowed_books.assert_called_once()
+
+def test_return_book(library, sample_book):
+    library.borrowed_books_db.borrowed_books_data = {sample_book.get_book_id(): {"user_id": "user1", "date": "2023-01-01"}}
+    result = library.return_book(sample_book)
+    assert result is True
+    library.borrowed_books_db.update_borrowed_books.assert_called_once()
